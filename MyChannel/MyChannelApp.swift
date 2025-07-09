ff1891b6a03d11ff464786bf63e36edd17157b0d
keontapeat <keontapeat@gmail.com>
2025-07-09 16:42:34 -0400

    Initial Commit

---
+++ MyChannel/MyChannelApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyChannelApp.swift
+//  MyChannel
+//
+//  Created by Keonta  on 7/9/25.
+//
+
+import SwiftUI
+
+@main
+struct MyChannelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
